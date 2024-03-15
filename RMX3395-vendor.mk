f4a4357d4c5199ce0d3b549abf64236fa6ca119c
Sidharth <157048502+arteryring@users.noreply.github.com>
2024-03-15 19:45:33 +0530

    RMX3395: Update RMX3395-vendor.mk

--- RMX3395-vendor.mk
+++ RMX3395-vendor.mk
@@ -5,6 +5,12 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/realme/RMX3395
 
+ODM_MANIFEST_FILES += \
+    vendor/realme/RMX3395/proprietary/vendor/odm/etc/vintf/manifest_dsds.xml \
+    vendor/realme/RMX3395/proprietary/vendor/odm/etc/vintf/manifest_qsqs.xml \
+    vendor/realme/RMX3395/proprietary/vendor/odm/etc/vintf/manifest_ss.xml \
+    vendor/realme/RMX3395/proprietary/vendor/odm/etc/vintf/manifest_tsts.xml
+
 PRODUCT_COPY_FILES += \
     vendor/realme/RMX3395/proprietary/etc/audio_effects.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_effects.conf \
     vendor/realme/RMX3395/proprietary/etc/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_SYSTEM)/etc/seccomp_policy/mediacodec.policy \
